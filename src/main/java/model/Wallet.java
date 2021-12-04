5a9ee7bcab70d1a31cfe8d02378933d8d8d374af
uyle1234 <linhconuong2k3@gmail.com>
2021-12-04 11:30:30 +0700

    uyle commits class wallet  and complete package model

--- src/main/java/model/Wallet.java
+++ src/main/java/model/Wallet.java
@@ -1,4 +1,30 @@
 package model;
 
 public class Wallet {
+    private int userId;
+    private double balance;
+
+    public Wallet() {
+    }
+
+    public Wallet(int userId, double balance) {
+        this.userId = userId;
+        this.balance = balance;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
 }
