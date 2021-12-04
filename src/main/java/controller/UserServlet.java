fd1b35a289955859d1d61c2427b9e60590eab4ea
hoangducviet1993 <ducviet12vl@gmail.com>
2021-12-05 03:43:12 +0700

    up lên xong đi ngủ

--- src/main/java/controller/UserServlet.java
+++ src/main/java/controller/UserServlet.java
@@ -1,15 +1,48 @@
 package controller;
 
+import dao.IUserDAO;
+import dao.UserDAO;
+import model.User;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet(name = "UserServlet", value = "/users")
 public class UserServlet extends HttpServlet {
+    IUserDAO userDAO = new UserDAO();
+    private RequestDispatcher requestDispatcher = null;
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
+        if (action == null) {
+            action = "";
+        }
+        try {
+            switch (action) {
 
+                default:
+                    showAllUser(request, response);
+                    break;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void showAllUser(HttpServletRequest request, HttpServletResponse response) throws SQLException, ClassNotFoundException, ServletException, IOException {
+        requestDispatcher = request.getRequestDispatcher("user/list_user.jsp");
+//        List<User> userList = new ArrayList<>();
+//        userList = userDAO.findAll();
+//        request.setAttribute("userList", userList);
+        requestDispatcher.forward(request, response);
     }
 
     @Override
---
+++ src/main/webapp/user/list_user.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Admin
+  Date: 12/5/2021
+  Time: 2:55 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%--<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+đây là list
+</body>
+</html>
