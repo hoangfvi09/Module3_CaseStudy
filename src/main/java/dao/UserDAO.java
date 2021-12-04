7c975ecdbd103a97bada81bb6b34b4aaec8e7830
hoangducviet1993 <ducviet12vl@gmail.com>
2021-12-04 23:14:49 +0700

    tao final User

--- src/main/java/dao/UserDAO.java
+++ src/main/java/dao/UserDAO.java
@@ -1,4 +1,72 @@
 package dao;
 
-public class UserDAO {
+import model.User;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
+
+public class UserDAO implements IUserDAO {
+    private final String INSERT_USERS_SQL = "INSERT INTO user" + " (username, password, email) VALUES " + " (?, ?, ?);";
+    private final String SELECT_USER_BY_USERNAME_PASS = "select * from user where username like ? and password like ?";
+    private final String SELECT_USER_BY_USERNAME = "select id, username, password, email, address, avatar from user where username like ?";
+    private final String SELECT_USER_BY_ID = "select * from user where id = ?";
+    private final String SELECT_ALL_USERS = "select * from user";
+    private final String DELETE_USERS_SQL = "delete from user where id = ?;";
+    private final String UPDATE_USERS_SQL = "update user set username = ?, password = ?,  email= ?, address =?, avatar = ? where id = ?";
+
+    public UserDAO() {
+
+    }
+
+    protected Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/demo20062?useSSL=false", "root", "123456");
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    @Override
+    public void insert(User user) throws SQLException, ClassNotFoundException {
+
+    }
+
+    @Override
+    public List<User> findAll() throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public User findById(int id) throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public boolean delete(int id) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean update(User user) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public List<User> findByName(String username) throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public User findByUsernamePass(String username, String password) throws SQLException, ClassNotFoundException {
+        return null;
+    }
 }
