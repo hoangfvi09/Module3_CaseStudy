4fac24d1de03ffe8b55141a6529e48504e21a69c
uyle1234 <linhconuong2k3@gmail.com>
2021-12-04 10:58:38 +0700

    uyle commit class orderdetail in package model

--- src/main/java/model/OrderDetail.java
+++ src/main/java/model/OrderDetail.java
@@ -1,4 +1,34 @@
 package model;
 
 public class OrderDetail {
+    private int orderId;
+    private int productId;
+    private int quantity;
+
+    public OrderDetail() {
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
