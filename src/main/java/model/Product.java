c55d139d6f00a475c9585d2d17f779b64b3586bf
uyle1234 <linhconuong2k3@gmail.com>
2021-12-04 11:03:11 +0700

    uyle commit class product in package model

--- src/main/java/model/Product.java
+++ src/main/java/model/Product.java
@@ -1,4 +1,80 @@
 package model;
 
 public class Product {
+    private int id;
+    private String name;
+    private int categoryId;
+    private String description;
+    private String image;
+    private float rate;
+    private int sold;
+
+    public Product() {
+    }
+
+    public Product(int id, String name, int categoryId, String description, String image, float rate, int sold) {
+        this.id = id;
+        this.name = name;
+        this.categoryId = categoryId;
+        this.description = description;
+        this.image = image;
+        this.rate = rate;
+        this.sold = sold;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public float getRate() {
+        return rate;
+    }
+
+    public void setRate(float rate) {
+        this.rate = rate;
+    }
+
+    public int getSold() {
+        return sold;
+    }
+
+    public void setSold(int sold) {
+        this.sold = sold;
+    }
 }
